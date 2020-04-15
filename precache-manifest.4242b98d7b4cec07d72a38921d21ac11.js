f4314b227c7b7a7d754dcf86e18cfb840077a759
regalforlyfe <rifal.ahmd2@gmail.com>
2020-04-15 15:06:32 +0700

    Updates

---
+++ precache-manifest.4242b98d7b4cec07d72a38921d21ac11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22ae02f0ba6134a580fbc1f71292c85",
+    "url": "/AHMADRIFAL-WEB3/index.html"
+  },
+  {
+    "revision": "74d037446f2443ea4a6a",
+    "url": "/AHMADRIFAL-WEB3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "15f97dc59b20d3434d35",
+    "url": "/AHMADRIFAL-WEB3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74d037446f2443ea4a6a",
+    "url": "/AHMADRIFAL-WEB3/static/js/2.17b349fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AHMADRIFAL-WEB3/static/js/2.17b349fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f97dc59b20d3434d35",
+    "url": "/AHMADRIFAL-WEB3/static/js/main.74340e3f.chunk.js"
+  },
+  {
+    "revision": "b5dd12d310f3c95ade36",
+    "url": "/AHMADRIFAL-WEB3/static/js/runtime-main.d7c45838.js"
+  }
+]);
